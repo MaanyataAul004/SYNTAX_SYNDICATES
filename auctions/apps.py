f4a4df30bd9d6a06fe3aa6fb5b70fea347ed189b
Ambarsariya007 <mvsbasra2006@gmail.com>
2024-09-05 00:42:14 +0530

    trial

--- auctions/apps.py
+++ auctions/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
 
 
+
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
 class AuctionsConfig(AppConfig):
     name = 'auctions'
+
+    def ready(self):
+        from django.contrib.sites.models import Site
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(create_default_site, sender=self)
+
+def create_default_site(sender, **kwargs):
+    Site.objects.update_or_create(
+        id=1,
+        defaults={
+            'domain': '127.0.0.1:8000',
+            'name': 'Your Site Name'
+        }
+    )
