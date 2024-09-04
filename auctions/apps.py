d2cfea607e1f6095eb160aa815de86a4ebef819e
Ambarsariya007 <mvsbasra2006@gmail.com>
2024-09-05 00:44:21 +0530

    trials

--- auctions/apps.py
+++ auctions/apps.py
@@ -1,23 +1,19 @@
 from django.apps import AppConfig
-
-
-
-from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from django.contrib.sites.models import Site
 
 class AuctionsConfig(AppConfig):
     name = 'auctions'
 
     def ready(self):
-        from django.contrib.sites.models import Site
-        from django.db.models.signals import post_migrate
+        from django.contrib.sites.models import Site  # Import inside ready to avoid circular imports
         post_migrate.connect(create_default_site, sender=self)
 
 def create_default_site(sender, **kwargs):
     Site.objects.update_or_create(
         id=1,
         defaults={
-            'domain': '127.0.0.1:8000',
+            'domain': '127.0.0.1:8000',  # Replace with your actual domain
             'name': 'Your Site Name'
         }
     )
