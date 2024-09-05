00c9c2271672112e17f97eabbdc28b4e6ea38b46
Ambarsariya007 <mvsbasra2006@gmail.com>
2024-09-05 16:48:39 +0530

    migration fixes

--- Dmanage/models.py
+++ Dmanage/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-
 class Disaster_report(models.Model):
     Disaster_choices = [
         ('flood', 'Flood'),
@@ -26,4 +25,4 @@ class Disaster_report(models.Model):
     reported_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.disaster_type} at {self.location}"
+        return f"{self.disaster_type} happening at {self.location}"
